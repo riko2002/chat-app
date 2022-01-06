65a4a6afea9692dea6704eaeaea950daaa253330
riko2002 <riko6021@icloud.com>
2022-01-06 21:22:49 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validatas :name, presence: true
-  
+  validates :name, presence: true
+
 end
